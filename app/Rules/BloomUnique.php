9a370a8cda9dc0aff82544cf9fdae062ad28ba65
Abdelrahman Dwedar <abdelrahman.dwedar.7350@gmail.com>
2024-10-31 10:26:26 +0300

    feat: add bloom filter validation rule

---
+++ app/Rules/BloomUnique.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Redis;
+use App\Models\User;
+
+class BloomUnique implements ValidationRule
+{
+    protected $filterName;
+
+    public function __construct(string $filterName)
+    {
+        $this->filterName = $filterName;
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (Redis::executeRaw(['BF.EXISTS', $this->filterName, $value])) {
+            // Check database only if Bloom filter indicates possible existence
+            if (User::where('email', $value)->exists()) {
+                $fail('The :attribute has already been taken.');
+            }
+        }
+    }
+}
